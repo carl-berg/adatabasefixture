977ec76d6b6b7892f9aee14c3647016b4b3f03d0
Carl Berg <carl@carl-berg.se>
2024-01-17 11:22:22 +0100

    Test fixes to allow tests in parallell by pipeline (or locally)

--- ADatabaseFixture.FluentMigrator.Tests/Core/DatabaseFixture.cs
+++ ADatabaseFixture.FluentMigrator.Tests/Core/DatabaseFixture.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 using ADatabaseFixture.FluentMigrator.Tests.Migrations;
 using Xunit;
 
@@ -7,8 +7,10 @@ namespace ADatabaseFixture.FluentMigrator.Tests.Core
     public class DatabaseFixture : DatabaseFixtureBase, IAsyncLifetime
     {
         public DatabaseFixture()
-            : base(new SqlServerDatabaseAdapter(), FluentMigratorMigrator.Create<CreatePersonTable>(Database.SqlServer2016))
+            : base(new SqlServerDatabaseAdapter(databaseName: DatabaseName()), FluentMigratorMigrator.Create<CreatePersonTable>(Database.SqlServer2016))
         {
         }
+
+        private static string DatabaseName() => $"FluentMigrator_TestDatabase_{DateTime.Now:yyyy-MM-dd_HH-mm}";
     }
 }
--- ADatabaseFixture.GalacticWasteManagement.Tests/Core/DatabaseFixture.cs
+++ ADatabaseFixture.GalacticWasteManagement.Tests/Core/DatabaseFixture.cs
@@ -1,12 +1,15 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace ADatabaseFixture.GalacticWasteManagement.Tests.Core
 {
     public class DatabaseFixture : DatabaseFixtureBase, IAsyncLifetime
     {
         public DatabaseFixture()
-            : base(new SqlServerDatabaseAdapter(), GalacticWasteManagementMigrator.Create<DatabaseFixture>())
+            : base(new SqlServerDatabaseAdapter(databaseName: DatabaseName()), GalacticWasteManagementMigrator.Create<DatabaseFixture>())
         {
         }
+
+        private static string DatabaseName() => $"GWM_TestDatabase_{DateTime.Now:yyyy-MM-dd_HH-mm}";
     }
 }
